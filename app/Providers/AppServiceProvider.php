1e933df2b7f379b3911ee8ce6d9c8c909805ff97
Jonnatha <42788538+Jonnnatha@users.noreply.github.com>
2025-09-10 11:29:34 -0300

    Stop exposing user roles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,17 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Inertia::share([
-            'auth' => function () {
-                $u = auth()->user();
-                return $u ? [
-                    'user' => [
-                        'id' => $u->id,
-                        'name' => $u->name,
-                        'roles' => $u->getRoleNames(),
-                    ],
-                ] : null;
-            },
+        \Inertia\Inertia::share([
+            'auth' => fn () => [
+                'user' => auth()->user()
+                    ? ['id' => auth()->id(), 'name' => auth()->user()->name]
+                    : null,
+            ],
         ]);
     }
 }
